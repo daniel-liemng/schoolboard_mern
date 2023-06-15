c59a997b164f31011d42bcbc2fe47ce0807fffa0
Daniel <daniel.liemng@gmail.com>
2023-06-15 18:47:50 -0400

    search students

--- frontend/src/pages/instructor/StudentsPage.tsx
+++ frontend/src/pages/instructor/StudentsPage.tsx
@@ -10,6 +10,7 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
   Typography,
 } from '@mui/material';
 import { useGetInstructorAllStudentsQuery } from '../../hooks/instructorHooks';
@@ -25,6 +26,9 @@ const StudentsPage = () => {
     error,
   } = useGetInstructorAllStudentsQuery();
 
+  const [searchTerm, setSearchTerm] = useState('');
+  const [tempList, setTempList] = useState<User[]>();
+
   const [userModalOpen, setUserModalOpen] = useState(false);
   const [registeredCourseModalOpen, setRegisteredCourseModalOpen] =
     useState(false);
@@ -32,11 +36,22 @@ const StudentsPage = () => {
 
   console.log('AALL', allMyStudents);
 
+  console.log('search', searchTerm);
+
   return (
     <Box sx={{ p: '3rem' }}>
       <Typography variant='h4' align='center' sx={{ mb: '3rem' }}>
         My students
       </Typography>
+
+      <Box>
+        <TextField
+          fullWidth
+          label='Search Student'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </Box>
 
       {isLoading && <Loading />}
 
@@ -51,67 +66,74 @@ const StudentsPage = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {allMyStudents?.map((student: User, index: number) => (
-              <TableRow
-                key={index}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component='th' scope='row'>
-                  {student?.avatar ? (
-                    <img
-                      src={student?.avatar}
-                      alt='profile'
-                      style={{ width: 100, height: 100 }}
-                    />
-                  ) : (
-                    <Avatar sx={{ width: 30, height: 30 }} />
-                  )}
-                </TableCell>
-                <TableCell align='left'>{student.name}</TableCell>
-                <TableCell align='left'>{student.email}</TableCell>
-                <TableCell align='left'>
-                  <Box
-                    sx={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}
-                  >
-                    <Button
-                      type='button'
-                      onClick={() => {
-                        setSelectedUser(student);
-                        setUserModalOpen(true);
-                      }}
-                      variant='contained'
-                      size='small'
-                      color='info'
+            {allMyStudents
+              ?.filter(
+                (stu: User) =>
+                  stu.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                  stu.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                  searchTerm === ''
+              )
+              .map((student: User, index: number) => (
+                <TableRow
+                  key={index}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component='th' scope='row'>
+                    {student?.avatar ? (
+                      <img
+                        src={student?.avatar}
+                        alt='profile'
+                        style={{ width: 60, height: 60, borderRadius: '50%' }}
+                      />
+                    ) : (
+                      <Avatar sx={{ width: 30, height: 30 }} />
+                    )}
+                  </TableCell>
+                  <TableCell align='left'>{student.name}</TableCell>
+                  <TableCell align='left'>{student.email}</TableCell>
+                  <TableCell align='left'>
+                    <Box
+                      sx={{ display: 'flex', flexWrap: 'wrap', gap: '0.5rem' }}
                     >
-                      Info
-                    </Button>
-                    <UserInfoModal
-                      isModalOpen={userModalOpen}
-                      handleClose={() => setUserModalOpen(false)}
-                      user={selectedUser}
-                    />
+                      <Button
+                        type='button'
+                        onClick={() => {
+                          setSelectedUser(student);
+                          setUserModalOpen(true);
+                        }}
+                        variant='contained'
+                        size='small'
+                        color='info'
+                      >
+                        Info
+                      </Button>
+                      <UserInfoModal
+                        isModalOpen={userModalOpen}
+                        handleClose={() => setUserModalOpen(false)}
+                        user={selectedUser}
+                      />
 
-                    <Button
-                      type='button'
-                      onClick={() => {
-                        setSelectedUser(student);
-                        setRegisteredCourseModalOpen(true);
-                      }}
-                      variant='contained'
-                      size='small'
-                      color='secondary'
-                    >
-                      Registered course
-                    </Button>
-                    <UserRegisteredCourseModal
-                      isModalOpen={registeredCourseModalOpen}
-                      handleClose={() => setRegisteredCourseModalOpen(false)}
-                      user={selectedUser}
-                    />
-                  </Box>
-                </TableCell>
-              </TableRow>
-            ))}
+                      <Button
+                        type='button'
+                        onClick={() => {
+                          setSelectedUser(student);
+                          setRegisteredCourseModalOpen(true);
+                        }}
+                        variant='contained'
+                        size='small'
+                        color='secondary'
+                      >
+                        Registered course
+                      </Button>
+                      <UserRegisteredCourseModal
+                        isModalOpen={registeredCourseModalOpen}
+                        handleClose={() => setRegisteredCourseModalOpen(false)}
+                        user={selectedUser}
+                      />
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       </TableContainer>
