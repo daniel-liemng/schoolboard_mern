6c2adcad74ef9525188eacb2eed5d0ee005ec2e9
Daniel <daniel.liemng@gmail.com>
2023-09-08 16:11:54 -0400

    be: cookie

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,19 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,PUT,POST,DELETE,UPDATE,OPTIONS'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
